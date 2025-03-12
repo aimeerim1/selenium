f5a5918d4780c0f166afc38ee62467d884a3c932
aimeerim <aimeerim888@gmail.com>
2025-03-12 17:20:14 +0600

    first commentary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,5 +11,7 @@ public class Main {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
+//        WebElement input = driver.findElement(By.className());
+
         }
 }
